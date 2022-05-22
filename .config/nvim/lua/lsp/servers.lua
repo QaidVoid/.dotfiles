d96cb036d15915356ad1865bbfc2472971c94556
QaidVoid <qaidvoid@gmail.com>
2022-05-22 20:47:51 +0545

    nvim: update servers list

--- .config/nvim/lua/lsp/servers.lua
+++ .config/nvim/lua/lsp/servers.lua
@@ -10,46 +10,64 @@ vim.g.markdown_fenced_languages = {
 local root_pattern = lspconfig.util.root_pattern
 
 local servers = {
+  -- Bash language server
   bashls = {
     cmd = { "bash-language-server", "start" },
   },
+  -- C/C++
   clangd = {},
+  -- CSS
   cssls = {
     cmd = { "vscode-css-language-server", "--stdio" },
   },
+  -- Deno
   denols = {
     cmd = { "deno", "lsp" },
     root_dir = root_pattern("deno.json"),
   },
+  -- Go
   gopls = {
     cmd = { "gopls" },
   },
+  -- GraphQL
   graphql = {
     cmd = { "graphql-lsp", "server", "-m", "stream" },
   },
+  -- HTML
   html = {
     cmd = { "vscode-html-language-server", "--stdio" },
   },
+  -- Java
   jdtls = {},
+  -- JSON
   jsonls = {
     cmd = { "vscode-json-language-server", "--stdio" },
   },
+  -- LaTeX
+  texlab = {},
+  -- Nix
   rnix = {},
+  -- Python
   pyright = {
     cmd = { "pyright-langserver", "--stdio" },
   },
+  -- Solidity
   solang = {
     cmd = { "solang", "--language-server", "--target", "ewasm" },
   },
+  -- SQL
   sqls = {
     cmd = { "sqls" },
   },
+  -- Lua
   sumneko_lua = {
-    cmd = { vim.env.HOME .. "/.local/bin/lua/bin/lua-language-server" },
+    cmd = { os.getenv("HOME") .. "/.local/bin/lua/bin/lua-language-server" },
   },
+  -- TOML
   taplo = {
     cmd = { "taplo-lsp", "run" },
   },
+  -- TypeScript/JavaScript
   tsserver = {
     cmd = { "typescript-language-server", "--stdio" },
   },
